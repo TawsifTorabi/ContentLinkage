0b802c8f082b2182f6890cbbb476fb400a47033e
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-09-18 09:56:42 -0700

    Admistrator Login Page

---
+++ adminlogin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+        .align {
+           text-align:center;
+        }
+        .error {color: #FF0000;}
+        </style>
+    </head>
+
+    <body>
+
+     <h1 class="align">HI BOSS!! </h1>
+     <h1 class="align">PLEASE VERIFY IT'S YOU</h1>
+        
+    <div>
+        <form class="align" method="post" action="processAdminlogin.php">
+        Admin username: <input type="text" name="adminusername"><br><br>
+        Admin password: <input type="password" name="adminpassword"><br><br>
+        
+        <input type="submit" value="log in">
+        </form>
+        </div>
+
+    </body>
+</html>
