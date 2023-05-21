dfb0e6889f70311a647a7d7f71c59e22491102b0
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2023-05-21 19:59:12 +0600

    Models

---
+++ model/confirm_dialogue_html.php
@@ -0,0 +1,13 @@
+<div class="notific" style="display: none;">
+	<div class="yesnodialogue">
+		<center>
+			Do You Want To Delete </br>
+			<span id="deletename" style="font-weight: bold;">NAME</span>?</br>
+			<small id="notificationComboBoxText" style="font-weight: bold;color: red;"></small>
+			</br>
+			</br>
+			<button id="confirmdelete" onclick="">YES</button>
+			<button onclick="hideComboBox()" id="notnow">NO</button>
+		</center>
+	</div>
+</div>
